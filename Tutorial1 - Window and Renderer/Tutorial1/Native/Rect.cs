a0e5e7ba3cf4958b9a6c1da7e502e4cccdaef2fd
Sergio Alonso <samupo@noobogames.com>
2015-05-18 17:54:35 +0200

    Tutorial 1
    - Basic SDL
    - Window
    - Renderer
    - Texture drawing (basic)

---
+++ Tutorial1 - Window and Renderer/Tutorial1/Native/Rect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1.Native
+{
+    // S9.- Since we are going to need to specify the position and size of
+    // the thing we are drawing we will need a Rect
+    // If we use this implementation it will correspond directly to SDL's
+    // and we won't have to make castings and transformations between them
+    public struct Rect
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+        // Also I added a nice constructor for user experience
+        public Rect(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+    }
+}
